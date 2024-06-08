803b2f252dfc03839556b998ea522345d6354a87
Francine Hahn <fran_hahn@hotmail.com>
2024-06-08 10:47:35 -0300

    feat: connection to mysql with singleton approach

---
+++ src/db/connection.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from mysql.connector import Error
+from config.db_config import DB_CONFIG
+
+class DatabaseConnection:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(DatabaseConnection, cls).__new__(cls, *args, **kwargs)
+            cls._instance._connection = None
+        return cls._instance
+    
+    def connect(self):
+        if self._connection is None or not self.connection.is_connected():
+            try:
+                self.connection = mysql.connector.connect(**DB_CONFIG)
+                if self.connection.is_connected():
+                    print("The connection to MySQL has been established successfully!")
+            except Error as e:
+                print("Error while trying to connect to MySQL:", e)
+                raise Error(str(e)) from e
+        return self.connection
+
+    def close(self):
+        if self.connection.is_connected():
+            self.connection.close()
+            print("The connection to MySQL has been closed.")
