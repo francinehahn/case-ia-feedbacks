f5d3ac3e50b2ecfc66aafccceae16ba8b171b6da
Francine Hahn <fran_hahn@hotmail.com>
2024-06-08 10:48:21 -0300

    del: I deleted the singleton class because it will not be used anymore

--- src/singleton/singleton.py
+++
@@ -1,7 +0,0 @@
-class Singleton(type):
-    __instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if not cls.__instances:
-            cls.__instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls.__instances[cls]
