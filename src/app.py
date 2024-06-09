08ca847e3b7bd89002904b535bb753d227b2aabd
Francine Hahn <fran_hahn@hotmail.com>
2024-06-09 16:59:20 -0300

    feat: backgroundscheduler to execute the weekly report function and email sender using email package from python

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from mysql.connector import Error
 from marshmallow import ValidationError
 from flask import Flask, jsonify, request
+from apscheduler.schedulers.background import BackgroundScheduler
 from service.feedback_service import FeedbackService
 from repository.implementations.feedback_mysql import FeedbackMySQL
 from repository.implementations.requested_features_mysql import RequestedFeaturesMySQL
@@ -11,19 +12,11 @@ from repository.implementations.feature_codes_mysql import FeatureCodesMySQL
 from db.connection import DatabaseConnection
 from ai.implementations.command_r_plus import CommandRplus
 from email_sender.email_sender import EmailSender
-from flask_mail import BadHeaderError
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-
-# email config
-app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
-app.config['MAIL_PORT'] = 587  # SMTP port
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
-app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 
 @app.route('/feedbacks', methods=['POST'])
 def feedbacks():
@@ -61,40 +54,45 @@ def feedbacks():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-@app.route('/weeklySummary', methods=['GET'])
+
+# this function will call the weeklySummary endpoint all fridays at 6PM
 def weekly_summary():
-    try:
-        # database connection
-        db_connection = DatabaseConnection()
+    with app.app_context():
+        try:
+            # database connection
+            db_connection = DatabaseConnection()
 
-        # repository layer
-        feedback_repository = FeedbackMySQL(connection=db_connection)
-        requested_features_repository = RequestedFeaturesMySQL(connection=db_connection)
-        feature_codes_repository = FeatureCodesMySQL(connection=db_connection)
+            # repository layer
+            feedback_repository = FeedbackMySQL(connection=db_connection)
+            requested_features_repository = RequestedFeaturesMySQL(connection=db_connection)
+            feature_codes_repository = FeatureCodesMySQL(connection=db_connection)
 
-        # AI
-        llm = CommandRplus()
-        
-        # email sender
-        email_sender = EmailSender(app=app)
+            # AI
+            llm = CommandRplus()
+            
+            # email sender
+            email_sender = EmailSender()
 
-        # service layer
-        feedback_service = FeedbackService(
-            feedback_repository=feedback_repository,
-            requested_features_repository=requested_features_repository,
-            feature_codes_repository=feature_codes_repository,
-            llm=llm,
-            email_sender=email_sender
-        )
-        feedback_service.weekly_summary()
-
-        return jsonify({'message': 'The email has been sent successfully.'}), 200
-    except BadHeaderError as e:
-        return jsonify({"error": str(e)}), 500
-    except Error as e:
-        return jsonify({"error": str(e)}), 500
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
+            # service layer
+            feedback_service = FeedbackService(
+                feedback_repository=feedback_repository,
+                requested_features_repository=requested_features_repository,
+                feature_codes_repository=feature_codes_repository,
+                llm=llm,
+                email_sender=email_sender
+            )
+            feedback_service.weekly_summary()
+        except Error as e:
+            print(str(e))
+        except Exception as e:
+            print(str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    scheduler = BackgroundScheduler(daemon=True)
+    scheduler.add_job(weekly_summary, 'cron', day_of_week='fri', hour=18, minute=00)
+    scheduler.start()
+
+    try:
+        app.run(debug=True)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
--- src/email_sender/email_sender.py
+++ src/email_sender/email_sender.py
@@ -1,24 +1,33 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import os
+import smtplib
+from email.message import EmailMessage
 from dotenv import load_dotenv
-from flask_mail import Message, Mail, BadHeaderError
 
 load_dotenv()
 
-class EmailSender:
-    def __init__(self, app):
-        self.app = app
-
-    def send(self, message:str):
+class EmailSender():
+    def __init__(self):
+        # Email configuration
+        self.sender_email = os.getenv("MAIL_USERNAME")
+        self.sender_password = os.getenv("MAIL_PASSWORD")
+        self.recipient_email = os.getenv("EMAIL_RECEIVER")
+        self.smtp_server = os.getenv("MAIL_SERVER")
+        self.smtp_port = 587
+    
+    def send(self, email:str):
+        # Create the message
+        message = EmailMessage()
+        message.set_content(email)
+        message['From'] = self.sender_email
+        message['To'] = self.recipient_email
+        message['Subject'] = 'Feedback Weekly Report'
         try:
-            email_receivers_str = os.getenv('EMAIL_RECEIVERS')
-            email_receivers_list = email_receivers_str.split(',')
-            mail = Mail(self.app)
-            msg = Message('Feedback Weekly Report', sender=os.getenv('EMAIL_SENDER'), recipients=email_receivers_list)
-            msg.body = message
-            mail.send(msg)
-        except BadHeaderError as e:
-            raise BadHeaderError(str(e)) from e
-        except Exception as e:
-            raise Exception(str(e)) from e
+            smtp_obj = smtplib.SMTP(self.smtp_server, self.smtp_port)
+            smtp_obj.starttls()
+            smtp_obj.login(self.sender_email, self.sender_password)
+            smtp_obj.send_message(message)
+            smtp_obj.quit()
+            print('Email sent successfully.')
+        except smtplib.SMTPException as e:
+            print('Error sending email:', str(e))
