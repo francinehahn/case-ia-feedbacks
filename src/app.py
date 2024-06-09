7e0b6e0e7d66dfa8652591d125cb4512eccfa611
Francine Hahn <fran_hahn@hotmail.com>
2024-06-09 18:29:12 -0300

    feat: web page inside the templates folder

--- src/app.py
+++ src/app.py
@@ -2,8 +2,9 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from mysql.connector import Error
+import requests
 from marshmallow import ValidationError
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from apscheduler.schedulers.background import BackgroundScheduler
 from service.feedback_service import FeedbackService
 from repository.implementations.feedback_mysql import FeedbackMySQL
@@ -18,6 +19,42 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# web page
+@app.route('/')
+def index():
+    try:
+        # database connection
+        db_connection = DatabaseConnection()
+        
+        # repository layer
+        feedback_repository = FeedbackMySQL(connection=db_connection)
+        requested_features_repository = RequestedFeaturesMySQL(connection=db_connection)
+        feature_codes_repository = FeatureCodesMySQL(connection=db_connection)
+        
+        # AI
+        llm = CommandRplus()
+        
+        # email sender
+        email_sender = EmailSender()
+        
+        # service layer
+        feedback_service = FeedbackService(
+            feedback_repository=feedback_repository,
+            requested_features_repository=requested_features_repository,
+            feature_codes_repository=feature_codes_repository,
+            llm=llm,
+            email_sender=email_sender
+        )
+        report = feedback_service.feedbacks_report()
+        return render_template('index.html', report=report)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 422 
+    except Error as e:
+        return jsonify({"error": str(e)}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+# endpoint that checks for spans, inserts the feedback into the db and returns the sentiment analysis by the llm
 @app.route('/feedbacks', methods=['POST'])
 def feedbacks():
     try:
@@ -58,42 +95,8 @@ def feedbacks():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-
-@app.route('/feedbacks_report', methods=['GET'])
-def feedbacks_report():
-    try:
-        # database connection
-        db_connection = DatabaseConnection()
-        
-        # repository layer
-        feedback_repository = FeedbackMySQL(connection=db_connection)
-        requested_features_repository = RequestedFeaturesMySQL(connection=db_connection)
-        feature_codes_repository = FeatureCodesMySQL(connection=db_connection)
-        
-        # AI
-        llm = CommandRplus()
-        
-        # email sender
-        email_sender = EmailSender()
-        
-        # service layer
-        feedback_service = FeedbackService(
-            feedback_repository=feedback_repository,
-            requested_features_repository=requested_features_repository,
-            feature_codes_repository=feature_codes_repository,
-            llm=llm,
-            email_sender=email_sender
-        )
-        report = feedback_service.feedbacks_report()
-        return jsonify(report), 200
-    except ValueError as e:
-        return jsonify({"error": str(e)}), 422 
-    except Error as e:
-        return jsonify({"error": str(e)}), 500
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
     
-# this function will call the weeklySummary endpoint all fridays at 6PM
+# this function will be called every friday at 6PM to send the weekly report by email
 def weekly_summary():
     with app.app_context():
         try:
